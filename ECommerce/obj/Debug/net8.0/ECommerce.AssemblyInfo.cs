23a9ac870826e7bba3c8430a2a3229adc8c720ac
Zeyad-3amr <zeyad.3amr01@gmail.com>
2024-10-15 08:21:02 +0300

    Deleted appsettings file

--- ECommerce/obj/Debug/net8.0/ECommerce.AssemblyInfo.cs
+++ ECommerce/obj/Debug/net8.0/ECommerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a48a3df9b66831541047433138d8225860de52b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
